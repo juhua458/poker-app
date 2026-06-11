7960ce6e4ea2cc193249f01caf33cba83b771a0d
juhua458 <1007401048@qq.com>
2026-06-11 21:27:12 +0800

    V2.9.99: version bump

--- app/src/main/java/com/pokerhelper/app/HttpServerService.kt
+++ app/src/main/java/com/pokerhelper/app/HttpServerService.kt
@@ -123,7 +123,7 @@ class HttpServerService : Service() {
                                 put("timeSinceLast", timeSinceLast)
                                 put("error", capture.lastError)
                                 put("panelWidth", panelW)
-                                put("version", "2.9.98")
+                                put("version", "2.9.99")
                                 put("htmlSource", hotloadSource)
                                 put("chipStatus", capture.lastChipStatus)
                             }.toString()
