00a64292110ecfaaf533f3b569db54b61accd329
juhua458 <1007401048@qq.com>
2026-06-30 05:23:39 +0800

    v2.9.160 FloatingService

--- app/src/main/java/com/pokerhelper/app/FloatingService.kt
+++ app/src/main/java/com/pokerhelper/app/FloatingService.kt
@@ -482,7 +482,7 @@ class FloatingService : Service() {
         }
 
         tvStatus = TextView(this).apply {
-            text = "青云 v2.9.159"
+            text = "青云 v2.9.160"
             setTextColor(0xFFe8edf5.toInt())
             textSize = 9f
             setPadding(2, 0, 2, 0)
